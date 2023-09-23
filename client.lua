13f30f59e187fc14123e986c56fca0c4d1577008
thomappp <143356129+thomappp@users.noreply.github.com>
2023-09-23 11:21:13 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -4,16 +4,22 @@ local ShowNotification = function(text)
     DrawNotification(true, true)
 end
 
+Config = {
+    affectsVehicles = false,
+    enterModeSound = { name = "Power_Down", ref = "DLC_HEIST_HACKING_SNAKE_SOUNDS" },
+    exitModeSound = { name = "police_notification", ref = "DLC_AS_VNT_Sounds" }
+}
+
 RegisterNetEvent("blackout_script:toggle_mode")
 AddEventHandler("blackout_script:toggle_mode", function(boolean)
     SetArtificialLightsState(boolean)
-    SetArtificialLightsStateAffectsVehicles(false)
+    SetArtificialLightsStateAffectsVehicles(Config.affectsVehicles)
 
     if boolean then
         ShowNotification("~y~Blackout activé\n~s~Les lumières de la ville se sont éteintes.")
-        PlaySoundFrontend(-1, "Power_Down", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
+        PlaySoundFrontend(-1, Config.enterModeSound.name, Config.enterModeSound.ref, 1)
     else
         ShowNotification("~y~Blackout désactivé\n~s~Les lumières de la ville se sont allumées.")
-        PlaySoundFrontend(-1, "police_notification", "DLC_AS_VNT_Sounds", 1)
+        PlaySoundFrontend(-1, Config.exitModeSound.name, Config.exitModeSound.ref, 1)
     end
 end)
